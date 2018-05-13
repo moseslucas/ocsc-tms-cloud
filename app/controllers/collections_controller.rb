95fa45ad6197678619106463c72d4794d129e51c
moseslucas <mo@moseslucas.com>
2018-05-13 15:29:38 +0800

    Fixes default sorting

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,12 +4,12 @@ class CollectionsController < ApplicationController
   helper SmartListing::Helper
 
   def index
-    @status1 = 1
     collection_scope = Document.joins(:payments, :client, :destination)
     .cargo.not_cancelled
     .where(payments: {payment_type: "collect"})
     collection_scope = collection_scope.where(status1: params[:closed_payments] === "2" ? 2 : 1)
     collection_scope = collection_scope.cargo_search(params[:filter]) if params[:filter]
+    collection_scope = collection_scope.group(:id)
     @collection = smart_listing_create(
       :collections,
       collection_scope,
