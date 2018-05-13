78af25cb5a68ff6b431cd6541604f6cd9a6e04ab
moseslucas <mo@moseslucas.com>
2018-05-13 15:24:25 +0800

    Fixes default sort

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,15 +5,16 @@ class CollectionsController < ApplicationController
 
   def index
     @status1 = 1
-    collection_scope = Document.includes(:payments)
+    collection_scope = Document.joins(:payments, :client, :destination)
     .cargo.not_cancelled
     .where(payments: {payment_type: "collect"})
     collection_scope = collection_scope.where(status1: params[:closed_payments] === "2" ? 2 : 1)
-    # collection_scope = collection_scope.search(params[:filter]) if params[:filter]
+    collection_scope = collection_scope.cargo_search(params[:filter]) if params[:filter]
     @collection = smart_listing_create(
       :collections,
       collection_scope,
-      partial: "collections/list"
+      partial: "collections/list",
+      default_sort: {trans_date: "desc"}
     )
   end
 end
