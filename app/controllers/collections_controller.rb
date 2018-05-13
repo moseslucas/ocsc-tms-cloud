69456606f32bd835e820152aacb97b9bf4cb035f
moseslucas <mo@moseslucas.com>
2018-05-13 15:12:38 +0800

    Toggle Open & Closed Collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,10 +4,11 @@ class CollectionsController < ApplicationController
   helper SmartListing::Helper
 
   def index
+    @status1 = 1
     collection_scope = Document.includes(:payments)
     .cargo.not_cancelled
     .where(payments: {payment_type: "collect"})
-    .where.not(status1: 2)
+    collection_scope = collection_scope.where(status1: params[:closed_payments] === "2" ? 2 : 1)
     # collection_scope = collection_scope.search(params[:filter]) if params[:filter]
     @collection = smart_listing_create(
       :collections,
@@ -15,5 +16,4 @@ class CollectionsController < ApplicationController
       partial: "collections/list"
     )
   end
-
 end
