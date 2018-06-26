2c7774308173bd719c4117cb251826d04a3e96eb
moseslucas <mo@moseslucas.com>
2018-06-26 15:51:02 +0800

    Fixes client filter, added waybil cwb filter to Cargos by Branch

--- app/controllers/cargos_controller.rb
+++ app/controllers/cargos_controller.rb
@@ -2,6 +2,7 @@ class CargosController < ApplicationController
   include DocumentsHelper
   include SmartListing::Helper::ControllerExtensions
   helper SmartListing::Helper
+  before_action :set_filters, only: :index
 
   def index 
     # byebug
@@ -14,8 +15,14 @@ class CargosController < ApplicationController
     .not_cancelled
     .from_exact_branch(session[:branch])
     cargos_scope = cargos_scope.cargo_search(params[:filter]) if params[:filter]
-    if params[:status2] && params[:status2] != "ALL"
+    if @f_status2 && @f_status2 != ""
       cargos_scope = cargos_scope.where( documents: { status2: params[:status2] } )
+    end
+    if @f_client_id && @f_client_id != ""
+      cargos_scope = cargos_scope.where( client_id: @f_client_id )
+    end
+    if @f_cwb && @f_cwb != ""
+      cargos_scope = cargos_scope.where( documents: {ref_id: @f_cwb} )
     end
     @cargos = smart_listing_create(
       :cargos,
@@ -30,5 +37,11 @@ class CargosController < ApplicationController
   end
 
   private
+
+  def set_filters
+    @f_status2 = params[:status2]
+    @f_client_id = params[:client_id]
+    @f_cwb = params[:cwb]
+  end
 
 end
