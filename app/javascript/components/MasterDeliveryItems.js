34214a3af0a04d24daf331d28d10d0d370f58c7e
moseslucas <mo@moseslucas.com>
2018-07-01 20:14:07 +0800

    Frontend for actions of master deliveries

--- app/javascript/components/MasterDeliveryItems.js
+++ app/javascript/components/MasterDeliveryItems.js
@@ -3,11 +3,17 @@ import React from 'react'
 class MasterDeliveryItems extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      checkedItems: 0
+    }
   }
 
-  componentDidMount () {
-    const { items } = this.props
-    console.log('items: ', items)
+  checked (e) {
+    if (e.target.checked) {
+      this.setState({ checkedItems: this.state.checkedItems + 1 })
+    } else {
+      this.setState({ checkedItems: this.state.checkedItems - 1 })
+    }
   }
 
   tableHeader () {
@@ -22,6 +28,7 @@ class MasterDeliveryItems extends React.Component {
           <th> Description </th>
           <th> Destination </th>
           <th>  Amount </th>
+          <th />
         </tr>
       </thead>
     )
@@ -41,16 +48,51 @@ class MasterDeliveryItems extends React.Component {
             <td>{item.description}</td>
             <td>{item.destination}</td>
             <td>{item.amount}</td>
+            <td>
+              <div className='checkbox'>
+                <label><input
+                  className='checkbox'
+                  type='checkbox'
+                  onChange={ e => this.checked(e) } />
+                </label>
+              </div>
+            </td>
           </tr>
         )
       })
     } else {
       return (
         <tr>
-          <td colspan='8'> No Records </td>
+          <td colspan='9'> No Records </td>
         </tr>
       )
     }
+  }
+
+  actions () {
+    const { items } = this.props
+    const { checkedItems } = this.state
+    const allChecked = items.length === checkedItems
+    return (
+      <React.Fragment>
+        <div className='row'>
+          <div className='col-sm-8 col-xs-12 form-group' style={{display: 'flex'}}>
+            <button className='btn green btn-lg' disabled={allChecked} style={{width: 180}}>
+              <i className='fa fa-circle-o-notch' />
+              LACKING
+             </button>
+            <input className='input-lg form-control' disabled={allChecked}/>
+          </div>
+          <div className='col-sm-1' />
+          <div className='col-sm-3 col-xs-12 form-group'>
+            <button className='btn btn-lg btn-block green-jungle pull-right' disabled={!allChecked}>
+              <i className='fa fa-check' />
+              SET TO COMPLETE
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 
   render () { 
@@ -62,6 +104,7 @@ class MasterDeliveryItems extends React.Component {
             { this.records() }
           </tbody>
         </table>
+        { this.actions() }
       </React.Fragment>
     )
   }
